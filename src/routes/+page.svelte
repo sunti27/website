90f63bed6b7bb446d366b1d7bad9e4c422075673
sunti27 <sunti.git@gmail.com>
2024-02-11 14:25:08 +0100

    Initial commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,5 @@
+<div class="container h-full mx-auto flex justify-center items-center">
+	<div class="w-auto h-auto bg-surface-500 p-4 y-4 rounded">
+		<h1 class="text-4xl inline-block">Work in Progress</h1>
+	</div>
+</div>
